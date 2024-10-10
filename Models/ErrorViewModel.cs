1ac5abeca8f5f0809e6ee7a499b3b4d28be71b2f
Sam Riddle <swriddle@crimson.ua.edu>
2024-10-10 14:36:36 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Capstone_360s.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
